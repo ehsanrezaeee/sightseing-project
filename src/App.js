221f06503b9f96f75db8900256247a0b9d6d6db4
esi <ehsanrezaee@hotmail.com>
2023-02-28 23:22:26 +0330

    second commit

--- src/App.js
+++ src/App.js
@@ -7,19 +7,23 @@ import {
 import "./App.css";
 import Users from "./user/pages/User";
 import NewPlace from "./places/pages/NewPlace";
+import MainNavigation from "./shared/components/Navigation/MainNavigation";
 
 const App = () => {
   return (
     <Router>
-      <Switch>
-        <Route path="/" exact>
-          <Users />
-        </Route>
-        <Route path="/places/new" exact>
-          <NewPlace />
-        </Route>
-        <Redirect to="/" />
-      </Switch>
+      <MainNavigation />
+      <main>
+        <Switch>
+          <Route path="/" exact>
+            <Users />
+          </Route>
+          <Route path="/places/new" exact>
+            <NewPlace />
+          </Route>
+          <Redirect to="/" />
+        </Switch>
+      </main>
     </Router>
   );
 };
---
+++ src/shared/components/Navigation/MainNavigation.js
@@ -0,0 +1,24 @@
+import "./MainNavigation.css";
+import MainHeader from "./MainHeader";
+import { Link } from "react-router-dom";
+import NavLinks from "./NavLinks";
+
+const MainNavigation = () => {
+  return (
+    <MainHeader>
+      <button className="main-navigation__menu-btn">
+        <span />
+        <span />
+        <span />
+      </button>
+      <h1 className="main-navigation__title">
+        <Link to="/">Your Places</Link>
+      </h1>
+      <nav className="main-navigation__header-nav">
+        <NavLinks />
+      </nav>
+    </MainHeader>
+  );
+};
+
+export default MainNavigation;
